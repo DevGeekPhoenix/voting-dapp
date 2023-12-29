066cb017d6e2cc2d3c453de72b62c66a9ea408d6
Front Phoenix <ho3einzarrabi@gmail.com>
2023-12-29 17:40:26 +0330

    first commit

---
+++ contracts/Vote.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract VoteDapp {
+    uint256 public NextJsVotes = 0;
+    uint256 public NuxtJsVotes = 0;
+    uint16 constant MAX_COMMENT_LENGTH = 200;
+
+    struct Comment {
+        uint256 id;
+        address author;
+        string content;
+        uint256 timestamp;
+        bool isForNextJs;
+    }
+
+    struct Voter {
+        bool hasVoted;
+        bool voteForNextJs;
+    }
+
+    event NewVoteSubmitted(address indexed voter, bool voteForNextJs);
+
+    event CommentCreated(
+        uint256 id,
+        address author,
+        string content,
+        uint256 timestamp,
+        bool voteForNextJs
+    );
+
+    mapping(address => Voter) public voters;
+    mapping(address => Comment[]) public comments;
+
+    modifier onlyOneVote() {
+        require(!voters[msg.sender].hasVoted, "You can only vote once!");
+        _;
+    }
+
+    function vote(bool isForNextJs) external onlyOneVote {
+        if (isForNextJs) {
+            NextJsVotes++;
+        } else {
+            NuxtJsVotes++;
+        }
+
+        voters[msg.sender] = Voter(true, isForNextJs);
+        emit NewVoteSubmitted(msg.sender, isForNextJs);
+    }
+
+    function createComment(string memory _comment) public {
+        require(
+            bytes(_comment).length <= MAX_COMMENT_LENGTH,
+            "Comment is too long!"
+        );
+        require(
+            voters[msg.sender].hasVoted,
+            "You need to vote before commenting!"
+        );
+
+        Comment memory newComment = Comment({
+            id: comments[msg.sender].length,
+            author: msg.sender,
+            content: _comment,
+            timestamp: block.timestamp,
+            isForNextJs: voters[msg.sender].voteForNextJs
+        });
+
+        comments[msg.sender].push(newComment);
+        emit CommentCreated(
+            newComment.id,
+            newComment.author,
+            newComment.content,
+            newComment.timestamp,
+            newComment.isForNextJs
+        );
+    }
+
+    function getVotes() public view returns (uint256[2] memory) {
+        return [NextJsVotes, NuxtJsVotes];
+    }
+}
